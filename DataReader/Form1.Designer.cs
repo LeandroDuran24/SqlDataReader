3fb553b6232fb9ec9d80e5f8cb6ae443348e852d
Leandro <Leandro@LEANDRODURAN>
2017-01-21 10:18:22 -0400

    sqlDataReader

--- DataReader/Form1.Designer.cs
+++ DataReader/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ClavetextBox = new System.Windows.Forms.TextBox();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.ApellidotextBox = new System.Windows.Forms.TextBox();
+            this.CiudadtextBox = new System.Windows.Forms.TextBox();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.Salirbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clave Vendedor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ciudad";
+            // 
+            // ClavetextBox
+            // 
+            this.ClavetextBox.Location = new System.Drawing.Point(110, 28);
+            this.ClavetextBox.Name = "ClavetextBox";
+            this.ClavetextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClavetextBox.TabIndex = 4;
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(109, 57);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(100, 20);
+            this.NombretextBox.TabIndex = 5;
+            // 
+            // ApellidotextBox
+            // 
+            this.ApellidotextBox.Location = new System.Drawing.Point(109, 84);
+            this.ApellidotextBox.Name = "ApellidotextBox";
+            this.ApellidotextBox.Size = new System.Drawing.Size(100, 20);
+            this.ApellidotextBox.TabIndex = 6;
+            // 
+            // CiudadtextBox
+            // 
+            this.CiudadtextBox.Location = new System.Drawing.Point(109, 113);
+            this.CiudadtextBox.Name = "CiudadtextBox";
+            this.CiudadtextBox.Size = new System.Drawing.Size(100, 20);
+            this.CiudadtextBox.TabIndex = 7;
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Location = new System.Drawing.Point(242, 28);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Buscarbutton.TabIndex = 8;
+            this.Buscarbutton.Text = "Buscar";
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            this.Buscarbutton.Click += new System.EventHandler(this.Buscarbutton_Click);
+            // 
+            // Salirbutton
+            // 
+            this.Salirbutton.Location = new System.Drawing.Point(242, 53);
+            this.Salirbutton.Name = "Salirbutton";
+            this.Salirbutton.Size = new System.Drawing.Size(75, 23);
+            this.Salirbutton.TabIndex = 9;
+            this.Salirbutton.Text = "Salir";
+            this.Salirbutton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 261);
+            this.Controls.Add(this.Salirbutton);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.CiudadtextBox);
+            this.Controls.Add(this.ApellidotextBox);
+            this.Controls.Add(this.NombretextBox);
+            this.Controls.Add(this.ClavetextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ClavetextBox;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.TextBox ApellidotextBox;
+        private System.Windows.Forms.TextBox CiudadtextBox;
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.Button Salirbutton;
     }
 }
 
--- DataReader/Form1.cs
+++ DataReader/Form1.cs
@@ -6,14 +6,42 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DataReader
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection sql = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=tutorial;Data Source=LeandroDuran");
+
+     
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Buscarbutton_Click(object sender, EventArgs e)
+        {
+            string cadSql = "Select * from vendedor where clave = '" + ClavetextBox.Text + "'";
+            SqlCommand comando = new SqlCommand(cadSql,sql);
+            sql.Open();
+
+            SqlDataReader leer = comando.ExecuteReader();
+            if (leer.Read() == true) {
+                NombretextBox.Text = leer["Nombre"].ToString();
+                ApellidotextBox.Text = leer["Apellido"].ToString();
+                CiudadtextBox.Text = leer["Ciudad"].ToString();
+            }
+            else {
+                MessageBox.Show("No se encontro Registro...");
+
+                NombretextBox.Text = "";
+                ApellidotextBox.Text = "";
+                CiudadtextBox.Text = "";
+                CiudadtextBox.Text = string.Empty;
+            }
+            sql.Close();
         }
     }
 }
